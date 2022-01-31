2cfbc10d9905bf0bf6b524b9eb93705da3792310
Qahwa <61147709+NOBUDAWN@users.noreply.github.com>
2022-01-31 23:51:57 +0300

    Just alg without norm prog

--- src/QuadraticEquation.java
+++ src/QuadraticEquation.java
@@ -5,12 +5,28 @@ public class QuadraticEquation {
         Scanner scanner = new Scanner(System.in);
         double A, B, C;
         double Dis; //дискриминант прям просит употребить нормальный ООП//
-        System.out.println("WE WORKS WITH STANDART QUADRATIC EQUATIONS: \n ax^2 + bx + c = 0 \n");
+        double route1, route2; //in internet many people in. them declare them in if-else, maube ur mistake//
+        System.out.println(" WE WORKS WITH STANDART QUADRATIC EQUATIONS: \n\n ax^2 + bx + c = 0 \n\n PRESS ENTER TO CONTINUE");
         System.out.println("Введите коэффициент A");
+        A = scanner.nextDouble();
         System.out.println("Введите коэффициент B");
+        B = scanner.nextDouble();
         System.out.println("Введите коэффициент C");
-        int a=scanner.nextInt();
-        System.out.println(a);
+        C = scanner.nextDouble();
+        Dis = (B * B) - (4 * A * C);
+
+        if(Dis>0){
+            route1 = (-B-Math.sqrt(Dis))/(2*A);
+            route2 = (-B+Math.sqrt(Dis))/(2*A);
+            System.out.println("ROUTES OF EQUATION \n FIRST:"+route1+"SECOND"+route2); //" SECOND: "+route2//
+        }
+        else if (Dis==0){
+            route1=-B/(2*A);
+            System.out.println("ROUTE OF EQUATION: "+route1);
+        } else{
+            System.out.println("THIS EQUATION HAS NO ROUTES");
+        }
+
     }
 }
 
