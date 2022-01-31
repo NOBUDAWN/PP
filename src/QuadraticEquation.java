21f8554636e3ec4c2bef9e732df2c216e74ac589
Qahwa <61147709+NOBUDAWN@users.noreply.github.com>
2022-01-31 23:31:06 +0300

    Just alg without norm prog

--- src/QuadraticEquation.java
+++ src/QuadraticEquation.java
@@ -6,7 +6,9 @@ public class QuadraticEquation {
         double A, B, C;
         double Dis; //дискриминант прям просит употребить нормальный ООП//
         System.out.println("WE WORKS WITH STANDART QUADRATIC EQUATIONS: \n ax^2 + bx + c = 0 \n");
-        System.out.println("Введите коэффициент ");
+        System.out.println("Введите коэффициент A");
+        System.out.println("Введите коэффициент B");
+        System.out.println("Введите коэффициент C");
         int a=scanner.nextInt();
         System.out.println(a);
     }
