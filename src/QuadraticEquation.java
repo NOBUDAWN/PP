350fbfb056960e363d6e02cd62b6fb41c1358f5f
Qahwa <61147709+NOBUDAWN@users.noreply.github.com>
2022-01-31 22:41:58 +0300

    Just alg

--- src/QuadraticEquation.java
+++ src/QuadraticEquation.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class QR {
+public class QuadraticEquation {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
     }
